2fbe056e463b3227dd69e13e820dcc4cc791a26e
aaafeig <geroy.ozhog08@mail.ru>
2025-04-11 12:47:18 +0300

    hw done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args)
     {
+        Author author1 = new Author("Сергей", "Пушкин");
+        Author author2 = new Author("Михаил", "Булгаков");
 
+        Book book1 = new Book("Евегений Онегин", author1.surname, 1820);
+        Book book2 = new Book("Собачье сердце", author2.surname, 1925);
+
+        System.out.println("book1.getYearPublication() = " + book1.getYearPublication());
+        book1.setYearPublication(1823);
+        System.out.println("book1.getYearPublication() = " + book1.getYearPublication());
+        System.out.println("book1.getString() = " + book1.getString());
+        System.out.println("book1.getAuthor() = " + book1.getAuthor());
+        System.out.println("book2.getString() = " + book2.getString());
+        System.out.println("book2.getAuthor() = " + book2.getAuthor());
+        System.out.println("book2.getYearPublication() = " + book2.getYearPublication());
     }
 }
