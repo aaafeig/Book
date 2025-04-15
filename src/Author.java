73d4e27c16679197312fadf919b998bd8d1212d6
Ozhogin.g <geroy.ozhog08@mail.ru>
2025-04-15 11:28:05 +0300

    fixed

--- src/Author.java
+++ src/Author.java
@@ -6,4 +6,20 @@ public class Author {
         this.firstName = firstName;
         this.surname = surname;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
         Author author1 = new Author("Сергей", "Пушкин");
         Author author2 = new Author("Михаил", "Булгаков");
 
-        Book book1 = new Book("Евегений Онегин", author1.surname, 1820);
-        Book book2 = new Book("Собачье сердце", author2.surname, 1925);
+        Book book1 = new Book("Евегений Онегин", author1, 1820);
+        Book book2 = new Book("Собачье сердце", author2, 1925);
 
         System.out.println("book1.getYearPublication() = " + book1.getYearPublication());
         book1.setYearPublication(1823);
         System.out.println("book1.getYearPublication() = " + book1.getYearPublication());
-        System.out.println("book1.getString() = " + book1.getString());
-        System.out.println("book1.getAuthor() = " + book1.getAuthor());
-        System.out.println("book2.getString() = " + book2.getString());
-        System.out.println("book2.getAuthor() = " + book2.getAuthor());
+        System.out.println("book1.getString() = " + book1.getTitle());
+        System.out.println("book1.getAuthor() = " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getSurname());
+        System.out.println("book2.getString() = " + book2.getTitle());
+        System.out.println("book2.getAuthor() = " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getSurname());
         System.out.println("book2.getYearPublication() = " + book2.getYearPublication());
     }
 }
